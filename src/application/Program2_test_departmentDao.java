3248195321e7c47751f36402ff107877648311e4
adrianoadacosta <adrianoadacosta@outlook.com>
2023-07-12 02:06:26 -0300

    Department Update implementation

--- src/application/Program2_test_departmentDao.java
+++ src/application/Program2_test_departmentDao.java
@@ -29,33 +29,21 @@ public class Program2_test_departmentDao {
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = " + newDepartment.getId());
 		
-		/*
-		
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
-
-		System.out.println("\n=== TEST 5: seller update =====");
-		seller = sellerDao.findById(1);
-		seller.setName("Martha Waine");
-		sellerDao.update(seller);
+		System.out.println("\n=== TEST 4: Department update =====");
+		Department dep1 = departmentDao.findById(5);
+		dep1.setName("Instrument");
+		departmentDao.update(dep1);
 		System.out.println("Update completed");
-
+		
+				
+		/*
 		System.out.println("\n=== TEST 6: seller delete =====");
 		System.out.println("Enter id for delete test: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
 		System.out.println("Delete completed");
 		
-		System.out.println("\n=== TEST 2: seller findByDepartment =====");
-		Department department = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(department);
-		for (Seller obj : list) {
-			System.out.println(obj);
-		}
-		
-		
-        */
+	        */
 		sc.close();
 	}
 }
